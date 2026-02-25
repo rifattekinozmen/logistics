5d738c9d2f97460ddd3c3fb272cd522a63e0a7be
Rifat Tekin Özmen <rifattekinozmen@gmail.com>
2026-02-25 11:54:11 +0300

    Remove FullTestDataSeeder from DatabaseSeeder to streamline data seeding process

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,6 @@ class DatabaseSeeder extends Seeder
             LocationSeeder::class,
             TaxOfficeSeeder::class,
             CompanySeeder::class,
-            FullTestDataSeeder::class,
         ]);
 
         // Admin kullanıcı oluştur
